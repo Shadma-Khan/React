8fd19890577f57ddc25841f77f9fc506c15792b4
Shadma-Khan <kshadma462@gmail.com>
2025-10-31 16:46:57 +0530

    02-Components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Cards from './components/Cards'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+
+  return (
+    <>
+    <Navbar />
+    <Cards />
+    </>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+        <div className="card"><h1>Eliza khan</h1>
+    <p>Lorem ipsum dolor sit amet.</p></div>
+    </div>
+  )
+}
+
+export default Cards
