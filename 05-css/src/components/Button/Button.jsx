6b595a643dff68e3c21d2f9c0eb4e72be1e22b09
Shadma-Khan <kshadma462@gmail.com>
2025-10-31 16:52:46 +0530

    05-CSS

---
+++ 05-css/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../Button.module.css'
+
+
+const Button = () => {
+  return (
+    <div>
+        <h5 className={styles.btn}>this is button</h5>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../Header.module.css'
+
+const Header = () => {
+  return (
+    <div ><h1 className={styles.card1}>Header</h1></div>
+  )
+}
+
+export default Header
