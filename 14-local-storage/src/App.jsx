6c1bca4958817d63c47ae7bf2941f2e788e3018b
Shadma-Khan <kshadma462@gmail.com>
2025-10-31 17:08:01 +0530

    14-Local Storage

---
+++ 14-local-storage/src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const App = () => {
+
+
+// topic 1 :- to clear localStoage
+  // localStorage.clear()
+  
+
+  // topic 2 :- to clear sessionStorage
+  // sessionStorage.clear()
+
+  // topic 3 :- to setItem in localStorage
+  // localStorage.setItem('user', 'abcd')
+
+
+  // topic 4 :- to getItem from localStorage
+  // const user = localStorage.getItem('user')
+  // console.log(user);
+
+
+  // topic 5 :- to setItem in localStorage
+  // localStorage.setItem('age', 20)
+  
+
+
+  // topic 6 :- to removeItem from localStorage
+  // localStorage.removeItem('age')
+
+
+  // topic 7 :- to covert object to string
+  // const user ={
+  //   user : "abcd",
+  //   age : 18,
+  //   city : 'indore'
+  // }
+
+  // localStorage.setItem('user', JSON.stringify(user))
+
+
+
+  // topic 8 :- to Recovert string to object (for console.log)
+  // const usera = JSON.parse(localStorage.getItem('user'))
+  // console.log(usera)
+
+
+  
+  // topic 9 :- removeItem
+// localStorage.removeItem('user')
+// sessionStorage.removeItem('key')
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
